08aae99b48fdd54366def133442aa811d8f6db85
Donatien Rolland <donatien@rollandmail.com>
2018-11-02 14:37:15 +0700

    modif last minute

--- app/javascript/components/downloadImage.js
+++ app/javascript/components/downloadImage.js
@@ -4,12 +4,13 @@ function insertImage(){
   const downloadImg = document.getElementById('photo-upload')
 
   function readUrl(input){
-      console.log(input)
 
     if (input.files && input.files[0]) {
       var reader = new FileReader();
       console.log(reader)
       reader.onload = function (e) {
+        console.log(e.status)
+        console.log(e.target.length)
         img.style.backgroundImage = "url(" + e.target.result + ")"
       }
       img.innerHTML = ""
@@ -19,7 +20,12 @@ function insertImage(){
 
   if (downloadImg != null ) {
     downloadImg.addEventListener('change', function(){
-      console.log(this)
+      let size = downloadImg.files[0].size/1024/1024
+      size = size.toFixed(2)
+
+      if (size > 10) {
+        alert('Le fichier est trop lourd : ' + size + " MB. Modifies la avant de la télécharger!!");
+      };
       readUrl(this)
     })
   }
--- app/javascript/components/egalWidth.js
+++ app/javascript/components/egalWidth.js
@@ -14,3 +14,13 @@ function getWidth(){
 }
 
 export { getWidth }
+
+
+$( document ).ready(function() {
+    var heights = $(".eql-height").map(function() {
+        return $(this).height();
+    }).get(),
+
+    maxHeight = Math.max.apply(null, heights);
+    $(".eql-height").height(maxHeight);
+});
